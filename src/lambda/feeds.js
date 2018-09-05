f4eebf28bc4404df912adada5e0a72072dfded15
coloradoweb <git@coloradoweb.com>
2018-09-04 19:44:38 -0600

    Update feeds.js

--- src/lambda/feeds.js
+++ src/lambda/feeds.js
@@ -1,6 +1,7 @@
 import Parser from 'rss-parser'
 
 const FEEDS = [
+  'https://coloradojustice.com/feed/',
   'http://blog.dynamicdrive.com/feed/',
   'https://css-tricks.com/feed/',
   'https://codepen.io/posts/feed',
