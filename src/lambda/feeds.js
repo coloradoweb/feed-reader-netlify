fffe435fb6bf5a99d443f6fdf4d0af01e44cf6ff
denvergeeks <git@coloradoweb.com>
2018-09-05 09:50:24 -0600

    Update feeds.js

--- src/lambda/feeds.js
+++ src/lambda/feeds.js
@@ -1,6 +1,7 @@
 import Parser from 'rss-parser'
 
 const FEEDS = [
+  'https://gwarp.com/rss/',
   'https://coloradojustice.com/feed/',
   'http://blog.dynamicdrive.com/feed/',
   'https://css-tricks.com/feed/',
