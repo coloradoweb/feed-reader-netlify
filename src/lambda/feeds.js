8045d265274a8916e198f9f7188f57f52195a1cb
denvergeeks <git@coloradoweb.com>
2018-09-05 09:44:17 -0600

    Update feeds.js

--- src/lambda/feeds.js
+++ src/lambda/feeds.js
@@ -1,11 +1,11 @@
 import Parser from 'rss-parser'
 
 const FEEDS = [
-  'https://gwarp.com/ghost/feed/',
   'https://coloradojustice.com/feed/',
   'http://blog.dynamicdrive.com/feed/',
   'https://css-tricks.com/feed/',
   'https://codepen.io/posts/feed',
+    'https://gwarp.com/ghost/feed/',
   'https://hnrss.org/frontpage',
   'http://github-trends.ryotarai.info/rss/github_trends_javascript_daily.rss'
 ]
